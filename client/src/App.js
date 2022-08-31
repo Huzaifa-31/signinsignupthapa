304f481e9dd301a0ebe7eb36309d713c72d79ac0
Huzaifa-31 <31huzafasameer@gmail.com>
2022-08-31 17:37:53 +0500

    31august happy birthday

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbarpage from "./components/Navbarpage";
+import SignIn from "./components/SignIn";
+function App() {
+  return (
+    <>
+      <Navbarpage />
+      <SignIn />
+    </>
+  );
+}
+
+export default App;
